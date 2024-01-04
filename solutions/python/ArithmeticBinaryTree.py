5e4180efb7a3caacc794f63f7e605bf6eb98d041
Luca Patrizi <82933856+patrizi-luca@users.noreply.github.com>
2024-01-04 11:40:46 +0100

    Create ArithmeticBinaryTree.py

---
+++ solutions/python/ArithmeticBinaryTree.py
@@ -0,0 +1,34 @@
+class Node:
+    def _init_(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+PLUS = "+"
+MINUS = "-"
+TIMES = "*"
+DIVIDE = "/"
+
+def evaluate(root):
+    if root.val == PLUS:
+        return evaluate(root.left) + evaluate(root.right)
+    elif root.val == MINUS:
+        return evaluate(root.left) - evaluate(root.right)
+    elif root.val == TIMES:
+        return evaluate(root.left) * evaluate(root.right)
+    elif root.val == DIVIDE:
+        return evaluate(root.left) / evaluate(root.right)
+    else:
+        return root.val
+
+# Example usage:
+tree = Node(TIMES)
+tree.left = Node(PLUS)
+tree.left.left = Node(3)
+tree.left.right = Node(2)
+tree.right = Node(PLUS)
+tree.right.left = Node(4)
+tree.right.right = Node(5)
+
+result = evaluate(tree)
+print(result)  # Output: 45
