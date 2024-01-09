8e01e74ae85c0d4c9da00fa6b5a074e6205c77ad
Luca Patrizi <82933856+patrizi-luca@users.noreply.github.com>
2024-01-09 17:33:12 +0100

    Create MinStack.py

---
+++ solutions/python/MinStack.py
@@ -0,0 +1,41 @@
+// Design a simple stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// getMin() -- Retrieve the minimum element in the stack.
+
+// Note: be sure that pop() and top() handle being called on an empty stack.
+
+class MinStack:
+    def _init_(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self):
+        if self.stack:
+            if self.stack[-1] == self.min_stack[-1]:
+                self.min_stack.pop()
+            return self.stack.pop()
+
+    def top(self):
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self):
+        if self.min_stack:
+            return self.min_stack[-1]
+
+# Example usage:
+x = MinStack()
+x.push(-2)
+x.push(0)
+x.push(-3)
+print(x.getMin())  # Output: -3
+x.pop()
+print(x.top())     # Output: 0
